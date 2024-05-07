4b34c5cf6353f81681699f3b30a3878547684d9a
Milan-Dhamala <dhamalamilan3@gmail.com>
2024-05-07 17:22:41 +0545

    app.jsx modified

--- client-new/src/App.jsx
+++ client-new/src/App.jsx
@@ -1,7 +1,7 @@
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
-import {Register} from "./pages/register/Register";
+import Register from "./pages/register/Register";
 import {
   BrowserRouter as Router,
   Routes,
@@ -14,21 +14,14 @@ import { AuthContext } from "./context/AuthContext";
 function App() {
   const { user } = useContext(AuthContext);
   return (
-    
-      <Router>
-        <Routes>
-        <Route exact path="/">
-          {user ? <Home /> : <Register />}
-        </Route>
-        <Route path="/login">{user ? <Navigate to="/" /> : <Login />}</Route>
-        <Route path="/register">
-          {user ? <Navigate to="/" /> : <Register />}
-        </Route>
-        <Route path="/profile/:username">
-          <Profile />
-        </Route>
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route path="/" element={user ? <Navigate to="/home" /> : <Register />} />
+        <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+        <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
+        <Route path="/profile/:username" element={<Profile />} />
+      </Routes>
+    </Router>
   );
 }
 
